29be2acc43ad841cd4e590c4fbee597225951f1f
Dawid Szymaniak <69818774+dawid-sz@users.noreply.github.com>
2022-09-07 12:33:21 +0200

    adding comments

--- assets/script.js
+++ assets/script.js
@@ -6,14 +6,14 @@ let navHeight = document.getElementById("main-nav").offsetHeight;
 let navbarLinks = document.querySelectorAll("nav a");
 
 window.addEventListener("scroll", e => {
-    scrollpos = window.scrollY;
+    scrollpos = window.scrollY; //declaring the scroll behavior
     navbarLinks.forEach(link => {
         let section = document.querySelector(link.hash);
         if (section.offsetTop <= scrollpos + 150 &&
             section.offsetTop + section.offsetHeight > scrollpos + 150) {
-                link.classList.add("active");
+                link.classList.add("active"); //adding the background on the button from currently viewed page
             } else {
-                link.classList.remove("active");
+                link.classList.remove("active"); //removing the background from the button
             }
     });
 });
